df0830d9ba826336b59ce8a3d26b1d16630c1e1d
Szymon Konieczny <szymon_konieczny@o2.pl>
2023-08-20 17:14:41 +0200

    const gallery added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+const gallery = document.querySelector(".gallery");
 let galleryListArray = [];
 
 galleryItems.forEach((galleryItem) => {
